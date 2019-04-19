448fd66b685283e68d00766cef6d2ef31686e616
jaskaran121 <jaskaran310193@gmail.com>
2019-04-18 23:01:13 -0400

    New Commit

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -3,12 +3,48 @@ import React, { Component } from "react";
 class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { data: { title: "", body: "" } };
   }
+
+  handleChange = e => {
+    const tempData = { ...this.state.data };
+    tempData[e.target.name] = e.target.value;
+    this.setState({ data: tempData });
+  };
+
+  handleSubmit = () => {
+    console.log(this.state.data);
+  };
+
   render() {
     return (
       <div>
-        <h>Add Posts</h>
+        <h3>Add Posts</h3>
+        <form>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label>Body:</label>
+            <br />
+            <textarea
+              name="body"
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
+          </div>
+          <br />
+          <button type="submit" onSubmit={this.handleSubmit}>
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
